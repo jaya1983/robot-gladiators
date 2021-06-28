8f5b6ffbefe48582b31c1df009700021d3398c0b
Jaya <jaya.vedantam@gmail.com>
2021-06-28 13:58:01 -0700

    java script

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+
+function fight() {
+  window.prompt("What is your robot's name?");
+
+  window.alert("The fight has begun!");
+}
+//fight();
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+window.alert(playerName);
+console.log(playerName);
